60c8ad03b8d3d56505d474a6107d11975f9d6da3
dibek <dibekpoudyal@gmail.com>
2024-08-05 21:04:47 +0545

    work on find script

--- project/search.sh
+++ project/search.sh
@@ -5,16 +5,20 @@
 # get input from user
 read -rp "Enter a word to search: " word
 
+# create a destination folder if it doesn't exist
+if [ ! -d found ]; then
+  mkdir found
+fi
 
 # loop over all files in current directory
 for file in *; do
   # check for files and folders
   if [ -f "$file" ]; then
-
     # look for the word in the file
     match=$(grep -in "$word" "$file")
     if [ -n "$match" ]; then
       echo "found in $file: $match"
+      mv "$file" found/
     fi
   else
     echo ""
